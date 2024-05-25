a5fa3be1cb98e261229eb4cabe8aca874a0054f8
Васютинский Глеб~ <g.v.vass@gmail.com>
2024-05-25 15:15:28 +0600

    Random code

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const now = new Date();
 
-        myText.innerHTML = `ONAY! ALA<br> AT ${now.getDate()}/${now.getMonth() + 1} ${now.getHours()}:${now.getMinutes()}<br>${number},#1265,100₸<br>http://qr.tha.kz/47601<br>.`;
+        myText.innerHTML = `ONAY! ALA<br> AT ${now.getDate()}/${now.getMonth() + 1} ${now.getHours()}:${now.getMinutes()}<br>${number},#${Math.floor(Math.random() * 9000) + 1000},100₸<br>http://qr.tha.kz/${Math.floor(Math.random() * 90000 + 10000)}<br>.`;
     });
 });
 
