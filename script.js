43ee147363fd297ab0fef03705945f0bb1c26151
Васютинский Глеб~ <g.v.vass@gmail.com>
2024-08-20 00:14:05 +0600

    100 > 120

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const now = new Date();
 
-        myText.innerHTML = `ONAY! ALA<br> AT ${now.getDate()}/${now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : (now.getMonth() + 1)} ${now.getHours()}:${now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()}<br>${number},#${Math.floor(Math.random() * 9000) + 1000},100₸<br>http://qr.tha.kz/${Math.floor(Math.random() * 90000 + 10000)}<br>.`;
+        myText.innerHTML = `ONAY! ALA<br> AT ${now.getDate()}/${
+            now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1
+        } ${now.getHours()}:${now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()}<br>${number},#${
+            Math.floor(Math.random() * 9000) + 1000
+        },120₸<br>http://qr.tha.kz/${Math.floor(Math.random() * 90000 + 10000)}<br>.`;
 
-        navigator.clipboard.writeText(`ONAY! ALA\nAT ${now.getDate()}/${now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : (now.getMonth() + 1)} ${now.getHours()}:${now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()}\n${number},#${Math.floor(Math.random() * 9000) + 1000},100₸\nhttp://qr.tha.kz/${Math.floor(Math.random() * 90000 + 10000)}\n.`);
+        navigator.clipboard.writeText(
+            `ONAY! ALA\nAT ${now.getDate()}/${
+                now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1
+            } ${now.getHours()}:${now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()}\n${number},#${
+                Math.floor(Math.random() * 9000) + 1000
+            },120₸\nhttp://qr.tha.kz/${Math.floor(Math.random() * 90000 + 10000)}\n.`
+        );
     });
 });
-
